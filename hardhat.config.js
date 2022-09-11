1a9923031d8d17a206ece4ad1b9ffb3cfe045555
Arthur Abeilice <afa7789@gmail.com>
2022-09-11 07:42:38 -0300

    mmmmm not sure

--- hardhat.config.js
+++ hardhat.config.js
@@ -12,81 +12,24 @@ require("dotenv").config()
  * @type import('hardhat/config').HardhatUserConfig
  */
 
-const MAINNET_RPC_URL =
-  process.env.MAINNET_RPC_URL ||
-  process.env.ALCHEMY_MAINNET_RPC_URL ||
-  "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
-const RINKEBY_RPC_URL =
-  process.env.RINKEBY_RPC_URL || "https://eth-rinkeby.alchemyapi.io/v2/your-api-key"
-const KOVAN_RPC_URL = process.env.KOVAN_RPC_URL || "https://eth-kovan.alchemyapi.io/v2/your-api-key"
-const POLYGON_MAINNET_RPC_URL =
-  process.env.POLYGON_MAINNET_RPC_URL || "https://polygon-mainnet.alchemyapi.io/v2/your-api-key"
-const GOERlI_RPC_URL =
-  process.env.GOERlI_RPC_URL || "https://eth-goerli.alchemyapi.io/v2/your-api-key"
+const POLYGON_MAINNET_RPC_URL = process.env.POLYGON_MAINNET_RPC_URL || "https://polygon-mainnet.alchemyapi.io/v2/your-api-key"
+
 const PRIVATE_KEY = process.env.PRIVATE_KEY
+
 // optional
-const MNEMONIC = process.env.MNEMONIC || "Your mnemonic"
 const FORKING_BLOCK_NUMBER = process.env.FORKING_BLOCK_NUMBER
 
 // Your API key for Etherscan, obtain one at https://etherscan.io/
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "Your etherscan API key"
 const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY || "Your polygonscan API key"
 const REPORT_GAS = process.env.REPORT_GAS || false
 
 module.exports = {
   defaultNetwork: "hardhat",
   networks: {
-    hardhat: {
-      // If you want to do some forking set `enabled` to true
-      forking: {
-        url: MAINNET_RPC_URL,
-        blockNumber: FORKING_BLOCK_NUMBER,
-        enabled: false,
-      },
-      chainId: 31337,
-    },
-    localhost: {
-      chainId: 31337,
-    },
-    kovan: {
-      url: KOVAN_RPC_URL,
-      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-      //accounts: {
-      //     mnemonic: MNEMONIC,
-      // },
-      saveDeployments: true,
-      chainId: 42,
-    },
-    rinkeby: {
-      url: RINKEBY_RPC_URL,
-      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-      //   accounts: {
-      //     mnemonic: MNEMONIC,
-      //   },
-      saveDeployments: true,
-      chainId: 4,
-    },
     mumbai: {
       url: "https://matic-mumbai.chainstacklabs.com",
-      accounts: [PRIVATE_KEY],
-    },
-    goerli: {
-      url: GOERlI_RPC_URL,
       accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-      //   accounts: {
-      //     mnemonic: MNEMONIC,
-      //   },
-      saveDeployments: true,
-      chainId: 5,
-    },
-    mainnet: {
-      url: MAINNET_RPC_URL,
-      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-      //   accounts: {
-      //     mnemonic: MNEMONIC,
-      //   },
-      saveDeployments: true,
-      chainId: 1,
+      chainId: 80001
     },
     polygon: {
       url: POLYGON_MAINNET_RPC_URL,
@@ -94,16 +37,12 @@ module.exports = {
       saveDeployments: true,
       chainId: 137,
     },
-    // mumbai:{
-    //   url: "https://matic-mumbai.chainstacklabs.com",
-    //   chainId: 80001,
-    //   accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-    // }
   },
   etherscan: {
     // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
     apiKey: {
       polygon: POLYGONSCAN_API_KEY,
+
     },
   },
   gasReporter: {
@@ -111,7 +50,6 @@ module.exports = {
     currency: "USD",
     outputFile: "gas-report.txt",
     noColors: true,
-    // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
   },
   contractSizer: {
     runOnCompile: false,
